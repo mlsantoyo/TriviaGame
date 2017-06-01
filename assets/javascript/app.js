0bc035427c508077c79a0c9b6c7ca2b347ea46aa
Marcy <mlsantoyo@att.net>
2017-06-01 00:18:34 -0400

    initial set up

---
+++ assets/javascript/app.js
@@ -0,0 +1,60 @@
+
+
+var questions = [{
+    question: "What is the main spirit used to make a Margarita?",
+    choices: ["Tequila", "Gin", "Vodka", "Lime"],
+    correctAnswer: 1
+}, {
+    question: "A mimosa is made by mixing orange juice and sparkling wine. What two ingredients make a bellini?",
+    choices: ["White wine & OJ", "Sparkling wine & peach nectar", "Sparkling water & any citrus", "Red wine & nectarines"],
+    correctAnswer: 2
+}, {
+    question: "Which drink do you shake rather than stir?",
+    choices: ["Manhattan", "Old Fashioned", "Ramos Gin Fizz", "Sazerac"],
+    correctAnswer: 3
+}, {
+    question: "The Mint Julep is the traditional drink at which sporting event?",
+    choices: ["Wimbledon", "Indy 500", "Preakness", "Kentucky Derby"],
+    correctAnswer: 4
+}, {
+    question: "If a spirit is '80 Proof' what is its percentage of alcohol?",
+    choices: ["80", "40", "20", "60"],
+    correctAnswer: 2
+
+ }, {
+    question: "Which plant is used to make tequila?",
+    choices: ["Agave", "Sugar Cane", "Wheat", "Prickly Pear Cactus"],
+    correctAnswer: 1
+    
+ }, {
+    question: "Which ingredient gives the Sazerac its licorice 'nose'?",
+    choices: ["Sambuca", "Absinthe", "Grappa", "Anisette"],
+    correctAnswer: 2 
+
+}, {
+    question: "Which beverage should be served at room temperature?",
+    choices: ["Belgian ale", "Punch", "Red Wine", "Rose Wine"],
+    correctAnswer: 3 
+
+}, {
+    question: "The glass known as a 'champagne coup' is said to have been modeled after whose breast?",
+    choices: ["Coco Chanel", "Princess Anne of Monaco", "Queen Isabela", "Marie Antoinette"],
+    correctAnswer: 4   
+
+}, { 
+    question: "A negroni is made with equal parts gin, sweet vermouth and which liqueur, also an apertif?",
+    choices: ["Campari", "Cinar", "Creme de violet", "Bitters"],
+    correctAnswer: 1 
+}];
+
+var showQuestion = 0;
+var correctAnswers = 0;
+var done = false;
+
+$(document).ready(function () {
+
+
+
+
+
+});
